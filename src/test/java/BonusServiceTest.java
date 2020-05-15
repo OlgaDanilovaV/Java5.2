04eca36f7a9f4c8f058d8e7f0feff508463853f6
OlgaDanilovaV <danilovaolga2235@gmail.com>
2020-05-15 17:57:05 +0300

    fixed #1

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+}
